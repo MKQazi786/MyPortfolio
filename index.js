b221220474b6e5c0fd053ba10d88bdf957e52b50
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2025-07-11 15:26:15 +0500

    fixed some content that css conflict

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ document.querySelector(".ham").addEventListener("click", () => {
     document.querySelector(".sidebar").classList.toggle("sidebarGo")
     if (document.querySelector(".sidebarGo")) {
         document.querySelector(".crossPng").style.display = 'none';
-        document.querySelector(".hamburger").style.display='inline'
+        setTimeout(()=>{
+            document.querySelector(".hamburger").style.display='inline'
+        },350)
     }else{
         setTimeout(()=>{
             document.querySelector(".crossPng").style.display = 'inline';
         },400)
         document.querySelector(".hamburger").style.display='none'; 
     }
+
 }
 
 );
